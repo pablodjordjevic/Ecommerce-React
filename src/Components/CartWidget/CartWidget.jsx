f4da348be14ced7868a7f0a8343dbf2a2755ace3
Pablo Djordjevic <pablodjordjevic@gmail.com>
2022-01-26 13:15:47 -0300

    tercer entrega

---
+++ src/Components/CartWidget/CartWidget.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faHeart, faShoppingCart, faUserAlt} from '@fortawesome/free-solid-svg-icons';
+
+
+
+const CartWidget = () => {
+    return (
+    <div>
+        <FontAwesomeIcon icon={faShoppingCart}/>
+        <FontAwesomeIcon icon={faUserAlt}/>  
+        <FontAwesomeIcon icon={faHeart}/>
+    </div>
+    )
+}
+
+export default CartWidget;
